862f9cc9d80d22c560b5fcdabf76c093c3de81d2
Noura Ismail <145156745+nouraismaill@users.noreply.github.com>
2024-07-15 00:08:17 +0300

    Update Contact.jsx

--- SeniorProject/frontend/src/pages/Contact.jsx
+++ SeniorProject/frontend/src/pages/Contact.jsx
@@ -13,7 +13,7 @@ const Contact = () => {
     try {
       await axios
         .post(
-          "http://localhost:5050/api/v1/message/send",
+          "https://revive-backend-j4x9.onrender.com/api/v1/message/send",
           { name, email, phone, message },
           {
             headers: { "Content-Type": "application/json" },
