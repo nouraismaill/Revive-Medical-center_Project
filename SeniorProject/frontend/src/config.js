155136a592186fe8f3ccc8d4b58075ecb5917ef9
Noura Ismail <145156745+nouraismaill@users.noreply.github.com>
2024-07-15 01:20:29 +0300

    Update config.js

--- SeniorProject/frontend/src/config.js
+++ SeniorProject/frontend/src/config.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "https://revive-backend-j4x9.onrender.com/api/v1";
+export const BASE_URL = "https://revive-backend-lgwl.onrender.com/api/v1";
 
 export const token = localStorage.getItem("token");
